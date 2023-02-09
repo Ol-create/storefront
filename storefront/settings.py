322de50d6ce1e8f7f7cc946f8f2dbb80b92bb84b
Ol-create <oluola96@gmail.com>
2023-02-09 12:54:08 +0100

    create view api for product

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
     'playground',
     'debug_toolbar',
     'store',
