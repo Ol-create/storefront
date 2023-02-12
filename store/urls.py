7d3ab17738d677ddff9cc4c54232cfabac290be8
Ol-create <oluola96@gmail.com>
2023-02-12 18:11:17 +0100

    Add serialization for relationship

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list),
     path('products/<int:id>/', views.product_details),
-    path('collections/<int:id>', views.collection_detail),
+    path('collections/<int:pk>', views.collection_detail, name='collection-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -13,7 +13,7 @@ from .serializers import ProductSerializer
 @api_view()
 def product_list(request):
     queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True)
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 @api_view()
@@ -21,3 +21,7 @@ def product_details(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
