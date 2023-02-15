3d98dad278a48ea0a13430f452897e0b9a427a60
Ol-create <oluola96@gmail.com>
2023-02-15 06:20:38 +0100

    Add delete method to product model

--- store/views.py
+++ store/views.py
@@ -20,16 +20,27 @@ def product_list(request):
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
+        serializer.save()
         return Response('ok')
         
    
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_details(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        # if product.orderitem_set.count() > 0:
+            #   return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response({"error":"Product deleted"}, status=status.HTTP_204_NO_CONTENT)       
 
 @api_view()
 def collection_detail(request, pk):
